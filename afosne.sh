0f97226e0b583c69fd8cdd24d50089c5f959eda1
KLafosne <109802795+KLafosne@users.noreply.github.com>
2022-12-16 17:49:26 +0800

    Update afosne.sh

--- afosne.sh
+++ afosne.sh
@@ -24,17 +24,126 @@ echo -e "# ${GREEN}电报${PLAIN}: https://t.me/uafosne                        #
 echo "#############################################################"
 echo ""
 
+if [ -f /etc/redhat-release ];then
+        OS='CentOS'
+    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
+        OS='Debian'
+    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
+        OS='Ubuntu'
+    else
+        echo "Not support OS, Please reinstall OS and retry!"
+        exit 1
+fi
+
+if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
+    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+    setenforce 0
+fi
+
+if [[ ${OS} == 'CentOS' ]];then
+	yum update -y
+    yum install screen -y
+else
+	apt-get update
+    apt install screen -y
+fi
+
 yellow "安装开始："
 #下载核心程序
-apt update
-apt install screen -y
 mkdir /afosne
+cd /afosne
 wget https://github.com/KLafosne/afosne/raw/master/afosne
-#设置运行权限
-chmod +x /afosne/afosne
-rm -f /afosne/config.json
-wget https://github.com/KLafosne/afosne/raw/main/config.json
+chmod +x afosne
+
+read -p "${yellow}输入你自己UUID:" UUID
+
+read -p "${yellow}输入你自己UUID:" PROT
+
+echo -e "# ${yellow}选择你需要的网络协议 #"
+echo -e "# ${yellow}1.ws      2.tcp      3.http    #"
+read  num                                
+
+if [ num == 1 ]
+then
+    network = ws
+elif [ num == 2 ]
+then
+   network = tcp
+elif [ num == 3 ]
+then
+   network = http
+else
+   echo "请输入上面的数字"
+fi
+echo -e "${Info} ${GreenBG} 你输入的配置信息为  端口：${port} UUID：${UUID} ${Font}"
+
+
+touch afosne/config.json
+	cat <<EOF > afosne/config.json
+{
+    "inbound": {
+        "port": ${PROT},
+        "protocol": "vmess",
+        "settings": {
+            "clients": [
+                {
+                    "id": "${UUID}",
+                    "level": 1,
+                    "alterId": 0
+                }
+            ]
+        },
+        "streamSettings": {
+            "network": "ws"
+        }
+    },
+    "outbound": {
+        "protocol": "freedom",
+        "settings": {}
+    },
+    "inboundDetour": [],
+    "outboundDetour": [
+        {
+            "protocol": "blackhole",
+            "settings": {},
+            "tag": "blocked"
+        }
+    ],
+    "routing": {
+        "strategy": "rules",
+        "settings": {
+            "rules": [
+                {
+                    "type": "field",
+                    "ip": [
+                        "0.0.0.0/8",
+                        "10.0.0.0/8",
+                        "100.64.0.0/10",
+                        "127.0.0.0/8",
+                        "169.254.0.0/16",
+                        "172.16.0.0/12",
+                        "192.0.0.0/24",
+                        "192.0.2.0/24",
+                        "192.168.0.0/16",
+                        "198.18.0.0/15",
+                        "198.51.100.0/24",
+                        "203.0.113.0/24",
+                        "::1/128",
+                        "fc00::/7",
+                        "fe80::/10"
+                    ],
+                    "outboundTag": "blocked"
+                }
+            ]
+        }
+    }
+}
+EOF
+
+
 # 开始后台运行
 screen afosne/afosne run
-#
+
+#yellow "内网穿透机子"
+
 green "安装完成"
